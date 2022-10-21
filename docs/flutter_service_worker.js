b58035930966404d197a312bcea5ac502a6fc8d8
Lucas Alves <l.alves001@gmail.com>
2022-10-20 23:00:50 -0300

    Updated web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b2fb0baa41c0702ce5fecea530872538",
-"index.html": "e2ebbace2734ec2c5d744204eb5abc41",
-"/": "e2ebbace2734ec2c5d744204eb5abc41",
-"main.dart.js": "abcdbbe794c43eb3efa78be9e787a537",
+"index.html": "206cf822ed86767e369f4677ba500b85",
+"/": "206cf822ed86767e369f4677ba500b85",
+"main.dart.js": "4cd7a5a35a0dc0d115ab6b1c1a7e299e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
